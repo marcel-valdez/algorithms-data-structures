5a9f21541201baa7c33e6431a21bda1aee30e147
Marcel Valdez <marcel.valdez@live.com>
2012-10-05 15:55:43 -0500

    Added modules for chapters

--- src/chapter_1/section2_exercises.rb
+++ src/chapter_1/section2_exercises.rb
@@ -1,80 +1,82 @@
 require_relative "../utils/point2d"
 require_relative "../utils/number_utils"
-class Section2Exercises
-  def point_distance_e121 n
-    # All numbers will be within a range 0,0 to n.n
-    # PointMap[x][y]: I know that the real Point2D(x,y) is going to be x/n, y/n) where x=[0,n] and y=[0,n]
-    points = Array.new(n) { Array.new(n) }
-    all_points = []
-    shortest = 2*n
-    # Loop 1 to N
-    (1..n).each {
-      # Create a point with random X and Y
-      x = rand 0..(n-1)
-      y = rand 0..(n-1)
-      point = Point2D.new(Float(x)/n, Float(y)/n)
-      all_points.push(point)
+module Chapter1
+  class Section2Exercises
+    def point_distance_e121 n
+      # All numbers will be within a range 0,0 to n.n
+      # PointMap[x][y]: I know that the real Point2D(x,y) is going to be x/n, y/n) where x=[0,n] and y=[0,n]
+      points = Array.new(n) { Array.new(n) }
+      all_points = []
+      shortest = 2*n
+      # Loop 1 to N
+      (1..n).each {
+        # Create a point with random X and Y
+        x = rand 0..(n-1)
+        y = rand 0..(n-1)
+        point = Point2D.new(Float(x)/n, Float(y)/n)
+        all_points.push(point)
 
-      if points[x][y].nil?
-        points[x][y] = point # Should I calculate until the end or while generating? Until the end
-      else
-        shortest = 0
-      end
-      # Compare it to all other points? <-- Inefficient:
-      #                                   Q: Can it be done more efficiently?
-      #                                   A: Yes, if points are ordered by X and Y, then it is only a matter of
-      #                                      comparing the closest points.
-      # If the calculated value is less than the stores lowest, set new length
-    }
+        if points[x][y].nil?
+          points[x][y] = point # Should I calculate until the end or while generating? Until the end
+        else
+          shortest = 0
+        end
+        # Compare it to all other points? <-- Inefficient:
+        #                                   Q: Can it be done more efficiently?
+        #                                   A: Yes, if points are ordered by X and Y, then it is only a matter of
+        #                                      comparing the closest points.
+        # If the calculated value is less than the stores lowest, set new length
+      }
 
-    if shortest != 0
-      # Calculate distance to all points
-      # loop all points
-      points.each_with_index { |row, y|
-        row.each_with_index { |current_point, x|
-          unless current_point.nil?
-            # only look for points that can be near enough to current point
-            y_range = (y+1)..Math.min(n-1, y + shortest)
-            x_range = (x+1)..Math.min(n-1, x + shortest)
-            # loop all points that can be shorter, except current
-            y_range.each { |other_y|
-              x_range.each { |other_x|
-                other_point = points[other_x][other_y]
-                unless (x == other_x and y == other_y) or other_point.nil?
-                  distance = current_point.distance_to other_point
-                  shortest = distance if shortest > distance
-                  # (don't calculate points that are too far)
-                end
+      if shortest != 0
+        # Calculate distance to all points
+        # loop all points
+        points.each_with_index { |row, y|
+          row.each_with_index { |current_point, x|
+            unless current_point.nil?
+              # only look for points that can be near enough to current point
+              y_range = (y+1)..Math.min(n-1, y + shortest)
+              x_range = (x+1)..Math.min(n-1, x + shortest)
+              # loop all points that can be shorter, except current
+              y_range.each { |other_y|
+                x_range.each { |other_x|
+                  other_point = points[other_x][other_y]
+                  unless (x == other_x and y == other_y) or other_point.nil?
+                    distance = current_point.distance_to other_point
+                    shortest = distance if shortest > distance
+                    # (don't calculate points that are too far)
+                  end
+                }
               }
-            }
 
-          end
+            end
+          }
+        }
+      end
+      # return points and shortest length
+
+      return all_points, shortest
+    end
+
+    def range_intersect_e122 ranges = []
+      # It is similar to the points intersection, but for ranges.
+      intersected_pairs = []
+      # loop ranges from 0 to n-1
+      ranges.each_with_index { |range_i, i| # get range Ri
+                                            #   loop ranges from i+1 to n-1 -> So we don't compare to Ro..Ri-1 that already compared themselves to Ri
+        ranges.drop(i+1).each { |range_j| # get range Rj
+                                          #     determine if Ri intersects Rj
+                                          #       if so add Ri,Rj to results
+          intersected_pairs.push([range_i, range_j]) if range_i.intersects? range_j
         }
       }
+
+      intersected_pairs
     end
-    # return points and shortest length
 
-    return all_points, shortest
+    def include_intersect_e123 n, min, max
+      # TODO: Implement this
+    end
+
   end
-
-  def range_intersect_e122 ranges = []
-    # It is similar to the points intersection, but for ranges.
-    intersected_pairs = []
-    # loop ranges from 0 to n-1
-    ranges.each_with_index { |range_i, i|# get range Ri
-      #   loop ranges from i+1 to n-1 -> So we don't compare to Ro..Ri-1 that already compared themselves to Ri
-      ranges.drop(i+1).each { |range_j|# get range Rj
-        #     determine if Ri intersects Rj
-        #       if so add Ri,Rj to results
-          intersected_pairs.push([range_i, range_j]) if range_i.intersects? range_j
-      }
-    }
-
-    intersected_pairs
-  end
-
-  def include_intersect_e123 n, min, max
-    # TODO: Implement this
-  end
-
 end
--- test/chapter_1/section1_exercises_test.rb
+++ test/chapter_1/section1_exercises_test.rb
@@ -1,226 +1,227 @@
 require "test/unit"
 require_relative "../../src/chapter_1/section1_exercises"
 require_relative "../test_helper"
+module Chapter1
+  class Section1Exercises_test < TestHelper
 
-class Section1Exercises_test < TestHelper
+    attr_accessor :rec_calls
 
-  attr_accessor :rec_calls
-
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-  def initialize(args)
-    super(args)
-    @target = Section1Exercises.new
-  end
-
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-  def teardown
-    # Do nothing
-  end
-
-  def check_string_result (result)
-    !result.nil? and result.instance_of? String
-  end
-
-  # Write a code fragment to print the transposition (rows and columns changed) of a two-dimensional array with M rows and N columns
-  def test_exercise_113
-    verify_method :exercise_113,
-                  :with => [{param: "1 2 3", expected: false},
-                            {param: "1 1 1", expected: true}]
-  end
-
-  # Write a method that takes an array 'a' of number values and number M as arguments and returns an array 'b' of length M whose b[i] us the number of times the integer i appeared in the argument array. If the values in a[] are all between 0 and M-1, the sume of the values in the returned array should be equal to a.length
-  def test_exercise_115
-    verify_method :exercise_115,
-                  :with => [{param: [0.8, 0.8], expected: true},
-                            {param: [0.1, 0.1], expected: true},
-                            {param: [0.9, 0.9], expected: true},
-                            {param: [1, 1], expected: false},
-                            {param: [0, 0], expected: false}]
-  end
-
-  # Give the value of exR1(6)
-  # Note: Implement exR1(6) in Ruby
-  def test_exercise_116
-
-    verify_method :exercise_116,
-                  :with => [{predicate: method(:check_string_result)}]
-
-    puts @target.exercise_116
-  end
-
-  # Give the values printed by the functions described in exercise 1.1.7
-  def test_exercise_117
-    verify_method :exercise_117, :with => [{predicate: method(:check_string_result)}]
-
-    puts @target.exercise_117
-
-  end
-
-  # Write a method that returns a string with the binary representation of N
-  def test_exercise_119
-    verify_method :exercise_119,
-                  :with => [{param: 1, expected: "1"},
-                            {param: 2, expected: "10"},
-                            {param: 3, expected: "11"},
-                            {param: 8, expected: "1000"},
-                            {param: 11, expected: "1011"}]
-  end
-
-  # Write a program that prints a Matrix with a header with the index of the columns
-  # and the index of the row, example:
-  # input: [3, 4]  output: " 12\n" +
-  #        [5, 6]          "134\n" +
-  #                        "256\n"
-  def test_exercise_1111
-    verify_method :exercise_1111,
-                  :with => [{param: [[true]], expected: " 1\n1*\n"},
-                            {param: [[false]], expected: " 1\n1 \n"},
-                            {param: [[true, false]], expected: " 12\n1* \n"},
-                            {param: [[true, false], [true, false]], expected: " 12\n1* \n2* \n"}]
-  end
-
-  # Write a method that receives a matrix NxM and transposes rows into columns MxN
-  def test_exercise_1113
-    verify_method :exercise_1113,
-                  :with => [{param: [[0]], expected: [[0]]},
-                            {param: [[0, 1]],
-                             expected: [[0],
-                                        [1]]},
-                            {param: [[0, 1],
-                                     [2, 3]],
-                             expected: [[0, 2],
-                                        [1, 3]]}]
-
-  end
-
-  # Write a method that calculates log2 of N, without using Math.log2
-  def test_exercise_1114
-    verify_method :exercise_1114,
-                  :with => [{param: 2, expected: Math.log2(2).truncate},
-                            {param: 3, expected: Math.log2(3).truncate},
-                            {param: 9, expected: Math.log2(9).truncate},
-                            {param: 90, expected: Math.log2(90).truncate}]
-  end
-
-  # Write a method that count the times a number appears in an array and
-  # stores this count in another array, example: [0, 1, 1] result: [1, 2]
-  def test_exercise_1115
-    verify_method :exercise_1115,
-                  :with => [{params: [[0], 1], expected: [1]},
-                            {params: [[0], 2], expected: [1, 0]},
-                            {params: [[1, 1], 2], expected: [0, 2]},
-                            {params: [[1, 2, 3, 3, 3, 4], 5], expected: [0, 1, 1, 3, 1]}]
-  end
-
-
-  def recursive_fibonacci (n)
-    return 0 if n == 0
-    return 1 if n == 1
-
-    recursive_fibonacci(n - 1) + recursive_fibonacci(n -2)
-  end
-
-  def calc_all_fibonacci (n)
-    result = Array.new n + 1
-    (0..n).each { |i| result[i] = recursive_fibonacci(i) }
-
-    result
-  end
-
-  # Write a program with better performance (5x faster) than calc_all_fibonacci
-  # to calculate all fibonacci numbers between 0 to N and store them in an array
-  def test_exercise_1119
-    expected_result = nil
-    time_span = time_block { expected_result = calc_all_fibonacci 20 }
-    verify_method :exercise_1119,
-                  :with => [{param: 20, expected: expected_result}]
-
-    actual_time_span = time_block { @target.exercise_1119 20 }
-
-    assert_true actual_time_span < time_span / 5
-  end
-
-  # Write a program that calculates the value ln(N!)
-  def test_exercise_1120
-    verify_method :exercise_1120,
-                  :with => [{param: 1, expected: Math.log(1).truncate},
-                            {param: 2, expected: Math.log(2).truncate},
-                            {param: 4, expected: Math.log(4 * 3 * 2).truncate},
-                            {param: 3, expected: Math.log(3 * 2).truncate}]
-  end
-
-  # 1.1.21 Write a program that reads in lines from standard input with each line
-  # containing a name and two integers and then uses printf to print a table
-  # with a column of the names, the integers, and result of dividing the first
-  # by the second, accurate to 3 decimal places. You could use a program like this
-  # to tabulate batting averages for baseball players or grades for students
-  def test_exercise_1121
-
-  end
-
-  # 1.1.22 Write a version of BinarySearch that uses the recursive rank() given on page 25 and traces the method calls. Each time the 
-  def test_exercise_1122
-    verify_method :exercise_1122,
-                  :with => [{params: [0, [0, 1, 2, 3, 4, 5]], expected: "lo: 0, hi: 5\n\tlo: 0, hi: 1\n"},
-                            {params: [5, [0, 1, 2, 3, 4, 5]], expected: "lo: 0, hi: 5\n\tlo: 3, hi: 5\n\t\tlo: 5, hi: 5\n"}]
-  end
-
-  # 1.1.24 Write a method that calculates the greatest common divisor, using Euclid's algorithm and
-  # use it to calculate the greatest common divisor for 1111111 and 1234567
-  def test_exercise_1124
-    verify_method :exercise_1124,
-                  :with => [{params: [1111111, 1234567], expected: 1},
-                            {params: [33 * 7, 33 * 23], expected: 33},
-                            {params: [41 * 13, 41 * 29], expected: 41}]
-
-  end
-
-  def binomial(n, k, p)
-    @rec_calls += 1
-
-    if n == 0 and k == 0
-      #print "1.0"
-      return 1.0
+    # Called before every test method runs. Can be used
+    # to set up fixture information.
+    def initialize(args)
+      super(args)
+      @target = Section1Exercises.new
     end
 
-    if n < 0 or k < 0
-      #print "0.0"
-      return 0.0
+    # Called after every test method runs. Can be used to tear
+    # down fixture information.
+    def teardown
+      # Do nothing
     end
-    #print "(((1 - #{p}) * "
-    left = ((1 - p) * binomial(n-1, k, p))
-    #print ") +\n (#{p} * "
-    right = (p * binomial(n-1, k-1, p))
-    #print "))"
 
-    left + right
-  end
+    def check_string_result (result)
+      !result.nil? and result.instance_of? String
+    end
 
-  # 1.1.27 Create a function that estimates the number of recursive calls of binomial
-  def test_exercise_1127
-    @rec_calls = 0
-    puts ""
-    binomial(10, 4, 0.25)
-    predicate = Proc.new { |actual|
-      Math.log10(actual).truncate == Math.log10(@rec_calls).truncate
-    }
+    # Write a code fragment to print the transposition (rows and columns changed) of a two-dimensional array with M rows and N columns
+    def test_exercise_113
+      verify_method :exercise_113,
+                    :with => [{param: "1 2 3", expected: false},
+                              {param: "1 1 1", expected: true}]
+    end
 
-    params = [10, 4]
-    verify_method :exercise_1127,
-                  :with => [{
-                                params: params,
-                                predicate: predicate
-                            }]   
-  end
+    # Write a method that takes an array 'a' of number values and number M as arguments and returns an array 'b' of length M whose b[i] us the number of times the integer i appeared in the argument array. If the values in a[] are all between 0 and M-1, the sume of the values in the returned array should be equal to a.length
+    def test_exercise_115
+      verify_method :exercise_115,
+                    :with => [{param: [0.8, 0.8], expected: true},
+                              {param: [0.1, 0.1], expected: true},
+                              {param: [0.9, 0.9], expected: true},
+                              {param: [1, 1], expected: false},
+                              {param: [0, 0], expected: false}]
+    end
 
-  # 1.1.28 Write a function that removes duplicates from an array
-  def test_exercise_1128
-    verify_method :exercise_1128,
-                  :with => [{param: [0, 0, 1, 2, 3, 3], expected: [0, 1, 2, 3]},
-                            {param: [0, 1, 2, 3], expected: [0, 1, 2, 3]},
-                            {param: [0, 0], expected: [0]},
-                            {param: [0], expected: [0]}]
+    # Give the value of exR1(6)
+    # Note: Implement exR1(6) in Ruby
+    def test_exercise_116
+
+      verify_method :exercise_116,
+                    :with => [{predicate: method(:check_string_result)}]
+
+      puts @target.exercise_116
+    end
+
+    # Give the values printed by the functions described in exercise 1.1.7
+    def test_exercise_117
+      verify_method :exercise_117, :with => [{predicate: method(:check_string_result)}]
+
+      puts @target.exercise_117
+
+    end
+
+    # Write a method that returns a string with the binary representation of N
+    def test_exercise_119
+      verify_method :exercise_119,
+                    :with => [{param: 1, expected: "1"},
+                              {param: 2, expected: "10"},
+                              {param: 3, expected: "11"},
+                              {param: 8, expected: "1000"},
+                              {param: 11, expected: "1011"}]
+    end
+
+    # Write a program that prints a Matrix with a header with the index of the columns
+    # and the index of the row, example:
+    # input: [3, 4]  output: " 12\n" +
+    #        [5, 6]          "134\n" +
+    #                        "256\n"
+    def test_exercise_1111
+      verify_method :exercise_1111,
+                    :with => [{param: [[true]], expected: " 1\n1*\n"},
+                              {param: [[false]], expected: " 1\n1 \n"},
+                              {param: [[true, false]], expected: " 12\n1* \n"},
+                              {param: [[true, false], [true, false]], expected: " 12\n1* \n2* \n"}]
+    end
+
+    # Write a method that receives a matrix NxM and transposes rows into columns MxN
+    def test_exercise_1113
+      verify_method :exercise_1113,
+                    :with => [{param: [[0]], expected: [[0]]},
+                              {param: [[0, 1]],
+                               expected: [[0],
+                                          [1]]},
+                              {param: [[0, 1],
+                                       [2, 3]],
+                               expected: [[0, 2],
+                                          [1, 3]]}]
+
+    end
+
+    # Write a method that calculates log2 of N, without using Math.log2
+    def test_exercise_1114
+      verify_method :exercise_1114,
+                    :with => [{param: 2, expected: Math.log2(2).truncate},
+                              {param: 3, expected: Math.log2(3).truncate},
+                              {param: 9, expected: Math.log2(9).truncate},
+                              {param: 90, expected: Math.log2(90).truncate}]
+    end
+
+    # Write a method that count the times a number appears in an array and
+    # stores this count in another array, example: [0, 1, 1] result: [1, 2]
+    def test_exercise_1115
+      verify_method :exercise_1115,
+                    :with => [{params: [[0], 1], expected: [1]},
+                              {params: [[0], 2], expected: [1, 0]},
+                              {params: [[1, 1], 2], expected: [0, 2]},
+                              {params: [[1, 2, 3, 3, 3, 4], 5], expected: [0, 1, 1, 3, 1]}]
+    end
+
+
+    def recursive_fibonacci (n)
+      return 0 if n == 0
+      return 1 if n == 1
+
+      recursive_fibonacci(n - 1) + recursive_fibonacci(n -2)
+    end
+
+    def calc_all_fibonacci (n)
+      result = Array.new n + 1
+      (0..n).each { |i| result[i] = recursive_fibonacci(i) }
+
+      result
+    end
+
+    # Write a program with better performance (5x faster) than calc_all_fibonacci
+    # to calculate all fibonacci numbers between 0 to N and store them in an array
+    def test_exercise_1119
+      expected_result = nil
+      time_span = time_block { expected_result = calc_all_fibonacci 20 }
+      verify_method :exercise_1119,
+                    :with => [{param: 20, expected: expected_result}]
+
+      actual_time_span = time_block { @target.exercise_1119 20 }
+
+      assert_true actual_time_span < time_span / 5
+    end
+
+    # Write a program that calculates the value ln(N!)
+    def test_exercise_1120
+      verify_method :exercise_1120,
+                    :with => [{param: 1, expected: Math.log(1).truncate},
+                              {param: 2, expected: Math.log(2).truncate},
+                              {param: 4, expected: Math.log(4 * 3 * 2).truncate},
+                              {param: 3, expected: Math.log(3 * 2).truncate}]
+    end
+
+    # 1.1.21 Write a program that reads in lines from standard input with each line
+    # containing a name and two integers and then uses printf to print a table
+    # with a column of the names, the integers, and result of dividing the first
+    # by the second, accurate to 3 decimal places. You could use a program like this
+    # to tabulate batting averages for baseball players or grades for students
+    def test_exercise_1121
+
+    end
+
+    # 1.1.22 Write a version of BinarySearch that uses the recursive rank() given on page 25 and traces the method calls. Each time the
+    def test_exercise_1122
+      verify_method :exercise_1122,
+                    :with => [{params: [0, [0, 1, 2, 3, 4, 5]], expected: "lo: 0, hi: 5\n\tlo: 0, hi: 1\n"},
+                              {params: [5, [0, 1, 2, 3, 4, 5]], expected: "lo: 0, hi: 5\n\tlo: 3, hi: 5\n\t\tlo: 5, hi: 5\n"}]
+    end
+
+    # 1.1.24 Write a method that calculates the greatest common divisor, using Euclid's algorithm and
+    # use it to calculate the greatest common divisor for 1111111 and 1234567
+    def test_exercise_1124
+      verify_method :exercise_1124,
+                    :with => [{params: [1111111, 1234567], expected: 1},
+                              {params: [33 * 7, 33 * 23], expected: 33},
+                              {params: [41 * 13, 41 * 29], expected: 41}]
+
+    end
+
+    def binomial(n, k, p)
+      @rec_calls += 1
+
+      if n == 0 and k == 0
+        #print "1.0"
+        return 1.0
+      end
+
+      if n < 0 or k < 0
+        #print "0.0"
+        return 0.0
+      end
+      #print "(((1 - #{p}) * "
+      left = ((1 - p) * binomial(n-1, k, p))
+      #print ") +\n (#{p} * "
+      right = (p * binomial(n-1, k-1, p))
+      #print "))"
+
+      left + right
+    end
+
+    # 1.1.27 Create a function that estimates the number of recursive calls of binomial
+    def test_exercise_1127
+      @rec_calls = 0
+      puts ""
+      binomial(10, 4, 0.25)
+      predicate = Proc.new { |actual|
+        Math.log10(actual).truncate == Math.log10(@rec_calls).truncate
+      }
+
+      params = [10, 4]
+      verify_method :exercise_1127,
+                    :with => [{
+                                  params: params,
+                                  predicate: predicate
+                              }]
+    end
+
+    # 1.1.28 Write a function that removes duplicates from an array
+    def test_exercise_1128
+      verify_method :exercise_1128,
+                    :with => [{param: [0, 0, 1, 2, 3, 3], expected: [0, 1, 2, 3]},
+                              {param: [0, 1, 2, 3], expected: [0, 1, 2, 3]},
+                              {param: [0, 0], expected: [0]},
+                              {param: [0], expected: [0]}]
+    end
   end
 end
