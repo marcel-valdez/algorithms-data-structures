f79b2479f253e1aa67a0e33b43b99356fb8268fc
Marcel Valdez <marcel.valdez@live.com>
2012-11-02 16:10:26 -0600

    Writing the binary search tree implemenation

--- src/utils/binary_search_tree.rb
+++ src/utils/binary_search_tree.rb
@@ -24,8 +24,57 @@ module Utils
       get_value(@root, key)
     end
 
+    # Gets the number of keys less than the given key
+    # @param [Object] key
+    # @return [Fixnum] the number of keys less than the given key
+    def rank(key)
+
+    end
+
+
+    # Gets the smallest key
+    # @return [Object] The smallest key
+    def min
+      min_node(@root)
+    end
+
+    # Gets the largest key
+    # @return [Object] The largest key
+    def max
+      max_node(@root)
+    end
+
+    # Gets the largest key less than or equal to key
+    # @return [Object] The largest key less than or equal to the key
+    # @param [Object] key
+    def floor(key)
+
+    end
+
+    # Gets the smallest key greater than or equal to key
+    def ceiling(key)
+
+    end
+
+    # Gets the key of the given rank
+    # @param [Object] rank
+    def select(rank)
+
+    end
+
+    # Deletes the smallest key
+    def delete_min
+      delete_min_node(@root)
+    end
+
+    # Deletes the largest key
+    def delete_max
+      delete_max_node(@root)
+    end
+
     # @param [Object] key Remove key (and its value) from the tree
     def delete(key)
+      delete_node(@root, key)
     end
 
     # @param [Object] key Is there a value paired with key?
@@ -38,15 +87,40 @@ module Utils
       size == 0
     end
 
-    # @return [Fixnum] Number of key-value pairs in the table.
-    def size
+    # Gets the number of key-value pairs in the range. If no parameter is given,
+    # it counts all the keys.
+    # parameters: lo, hi. Example: size(1,5), counts all the keys greater than
+    # or equal to 1, and all keys less than or equal to 5.
+    # Giving no parameters is equivalent to: size(min, max)
+    # @return [Fixnum] Number of key-value pairs in the range.
+    def size(*args)
+      return range_size(args[0], args[1]) unless args.nil? or args.empty?
+
       node_size(@root)
     end
 
-    # @return [Object] All the keys in the table
-    def keys
+    # Gets all the keys in a given range. If no parameter is given, it gets
+    # all the keys.
+    # Parameters: lo, hi. Example: keys(1, 5), gets all the keys less than
+    # or equal to 5 and greater than or equal to 1.
+    # Giving no parameters is equivalent to: keys(min, max)
+    # @return [Object] All the keys in the range
+    def keys(*args)
+      return range_keys(args[0], args[1]) unless args.nil? or args.empty?
+
+      result = ::Utils::Queue.new
+      node_keys(@root, result)
+
+      result
     end
 
+    def node_keys(node, queue)
+      return if node.nil?
+
+      node_keys(node.left, queue)
+      queue.queue(node.key)
+      node_keys(node.right, queue)
+    end
 
     def each(&block)
       # in order
@@ -56,6 +130,84 @@ module Utils
     end
 
     private
+
+    def delete_node(node, key)
+      return nil if node.nil?
+      comparison = key <=> node.key
+
+      if comparison < 0 # key to delete is smaller than node's key
+        node.left = delete_node(node.left, key)
+      elsif comparison > 0 # key to delete is greater than node's key
+        node.right = delete_node(node.right, key)
+      else # node is the node we want to delete
+        return node.left if node.right.nil?
+        return node.right if node.left.nil?
+
+        tmp = node
+        if rand(2) == 0
+          node = min_node(tmp.right)
+          node.left = delete_max_node(tmp.left)
+        else
+          node = max_node(tmp.left)
+          node.right = delete_min_node(tmp.right)
+        end
+      end
+
+      node.size = node_size(node.left) + 1 + node_size(node.right)
+
+    end
+
+    def max_node(node)
+      return nil if node.nil?
+
+      key = node.key
+      current = node.right
+      until current.nil?
+        key = current.key
+        current = current.right
+      end
+
+      key
+    end
+
+    def min_node(node)
+      return nil if node.nil?
+
+      key = node.key
+      current = node.left
+      until current.nil?
+        key = current.key
+        current = current.left
+      end
+
+      key
+    end
+
+    def delete_max_node(node)
+      return node.left if node.right.nil?
+
+      node.right = delete_max_node(node.right)
+      node.size = node_size(node.left) + 1 + node_size(node.right)
+
+      node
+    end
+
+    def delete_min_node(node)
+      return node.right if node.left.nil?
+
+      node.left = delete_min_node(node.left)
+      node.size = node_size(node.left) + 1 + node_size(node.right)
+
+      node
+    end
+
+    def range_size(lo, hi)
+
+    end
+
+    def range_keys(lo, hi)
+
+    end
 
     # @param [BSTNode] node
     def node_size(node)
--- test/utils/binary_search_tree_test.rb
+++ test/utils/binary_search_tree_test.rb
@@ -13,8 +13,23 @@ module Utils
 
     test "It has correct API definition" do
       # Arrange
-      api = [:put, :get, :delete, :contains, :is_empty?, :size, :keys]
-      non_api = [:size=, :is_empty=, :keys=, :root, :root=]
+      api = [
+          :put,
+          :get,
+          :delete,
+          :contains,
+          :is_empty?,
+          :size,
+          :keys,
+          :min,
+          :max,
+          :floor,
+          :ceiling,
+          :select,
+          :rank,
+          :delete_min,
+          :delete_max]
+      non_api = [:size=, :is_empty=, :keys=, :root, :root=, :min=, :floor=]
 
       # Act
       target = BinarySearchTree.new
@@ -30,6 +45,83 @@ module Utils
       }
     end
 
+    def insert_values(target, range)
+      range.each { |value| target.put(value, value.to_s) }
+    end
+
+    test "Test it can get min" do
+      # Arrange
+      target = BinarySearchTree.new
+      insert_values(target, 1..5)
+      expected_min = (1..5).min
+
+      # Act
+      min = target.min
+
+      # Assert
+      assert_equal(expected_min, min)
+    end
+
+    test "Test it can get max" do
+      # Arrange
+      target = BinarySearchTree.new
+      insert_values(target, 1..5)
+      expected_max = (1..5).max
+
+      # Act
+      max = target.max
+
+      # Assert
+      assert_equal(expected_max, max)
+    end
+
+    def floor_test_helper(expected_floor, param, target)
+      floor = target.floor(param)
+
+      # Assert
+      assert_equal(expected_floor, floor)
+    end
+
+    #
+    #
+    test "Test it can get floor" do
+      # Arrange
+      target = BinarySearchTree.new
+      insert_values(target, [1, 2, 3, 5, 7, 8])
+      params = [{param: 6, expect: 5},
+                {param: 5, expect: 5},
+                {param: 0, expect: 1},
+                {param: 9, expect: 8}]
+
+      params.each { |pair|
+        param = pair[:param]
+        expected_floor = pair[:expect]
+
+        # Act
+        floor_test_helper(expected_floor, param, target)
+      }
+
+    end
+    #
+    #test "Test it can get ceiling" do
+    #  assert_fail_assertion()
+    #end
+    #
+    #test "Test it can select keys" do
+    #  assert_fail_assertion()
+    #end
+    #
+    #test "Test it can get the rank" do
+    #  assert_fail_assertion()
+    #end
+    #
+    #test "Test it can delete the largest key" do
+    #  assert_fail_assertion()
+    #end
+    #
+    #test "Test it can delete the smallest key" do
+    #  assert_fail_assertion()
+    #end
 
     test "Test it starts empty" do
       # Arrange
@@ -45,19 +137,20 @@ module Utils
     test "Test each to find node " do
       # Arrange
       target = BinarySearchTree.new
-      i=2
+      i=1
 
       # Act
       target.put 1, "1"
       target.put 2, "2"
 
       # Assert
-      target.keys.each { |node_value|
-        assert_equal i.to_s, node_value
+      target.keys.each { |node_key|
+        assert_equal i, node_key
         i+=1
       }
     end
 
+    # This is a story test for the basic functionality of the BST
     test "Test if it can add first node" do
       # Arrange
       target = BinarySearchTree.new
