68faecea33b068440f11da2c1f34ac6df1f1049e
Marcel Valdez <marcel.valdez@live.com>
2012-12-11 15:18:55 -0600

    Working on TrieST

---
+++ src/utils/trie_symbol_table.rb
@@ -0,0 +1,97 @@
+# encoding: utf-8
+require_relative 'queue'
+
+module Utils
+  # Symbol table with string keys, implemented using a ternary search
+  # trie (TST).
+  # Remark: Can't use a key that is the empty string ""
+  class TrieSymbolTable
+
+    def initialize
+      @size = 0
+      @root = TrieNode.new
+    end
+
+    include Enumerable
+
+    # @return [Fixnum] number of key-value pairs
+    def size
+      @size
+    end
+
+    # Is string key in the symbol table?
+    def contains?(key)
+      not get(key).nil?
+    end
+
+    # @return [Object] Get value associated with a given key
+    def get(key)
+      node= get_node(@root, key, 0)
+      return nil if node.nil?
+      node.value
+    end
+
+    # Insert string s into the symbol table.
+    def put(key, value)
+      @root = put_node(@root, key, value, 0)
+    end
+
+    # Deletes the key-value pair from the Trie matching the given key
+    def delete(key)
+
+    end
+
+    # @return [Enumerable] all keys in symbol table
+    def keys
+    end
+
+    # @return [String] Find and return longest prefix of s in TST
+    def longest_prefix_of
+    end
+
+    # @return [Enumerable] all keys starting with given prefix
+    def prefix_match
+    end
+
+    def each
+    end
+
+    # @return [Enumerable] all keys in subtrie rooted at x with given prefix
+    def collect
+    end
+
+    # @return [Enumerable] all keys matching given wilcard pattern
+    def wildcard_match
+    end
+
+    private
+    def put_node(node, key, value, idx)
+      node = TrieNode.new if node.nil?
+      if idx == key.length
+        node.value = value
+        @size += 1
+        return node
+      end
+
+      sub_key = key[idx].ord
+      node.next[sub_key] = put_node(node.next[sub_key], key, value, idx + 1)
+
+      node
+    end
+
+    def get_node(node, key, idx)
+      return nil if node.nil?
+      return node if key.size == idx
+      sub_key = key[idx].ord
+      get_node(node.next[sub_key], key, idx + 1)
+    end
+
+    class TrieNode
+      attr_accessor :value, :next
+
+      def initialize(size = 256)
+        @next = Array.new size
+      end
+    end
+  end
+end
---
+++ test/utils/trie_symbol_table_test.rb
@@ -0,0 +1,79 @@
+# encoding: utf-8
+
+require_relative "../test_helper"
+require_relative "../../src/utils/trie_symbol_table"
+require_relative "utils_test_helper"
+
+module Utils
+  class TrieSymbolTableTest < TestHelper
+    include UtilsTestHelper
+
+    def initialize(*arg)
+      super(*arg)
+      @target= nil
+    end
+
+    test "if it has correct API definition" do
+      # Arrange
+      api = [
+        :size, :contains?, :get, :put, :longest_prefix_of,
+        :each, :collect, :wildcard_match, :prefix_match, :keys,
+        :delete
+      ]
+      non_api = [:size= ]
+
+      # Act
+      @target = TrieSymbolTable.new
+
+      # Assert
+      assert_api(api, non_api)
+    end
+
+    test "test if it is initialized correctly" do
+      # Arrange
+      @target = nil
+
+      # Act
+      @target = TrieSymbolTable.new
+
+      # Assert
+      assert_equal 0, @target.size
+
+      # Clean
+      @target = nil
+    end
+
+    test "test if it can put a new key-value (put/size/contains?)" do
+      # Arrange
+      @target = TrieSymbolTable.new
+
+      # Act
+      @target.put("key", 1)
+      value = @target.get("key")
+      contained = @target.contains? "key"
+
+      # Assert
+      assert_equal 1, @target.size
+      assert_equal 1, value
+      assert contained
+
+      # Clean
+      @target = nil
+    end
+
+    test "test if it it can put several values and retrieve all" do
+      # Arrange
+      @target = TrieSymbolTable.new
+
+      (1)
+      # Act
+
+
+      # Assert
+
+
+      # Clean
+
+    end
+  end
+end
