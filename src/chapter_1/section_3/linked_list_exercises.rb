5ef633fceac89ba5a9b462585c65e84666a4327e
Marcel Valdez <marcel.valdez@live.com>
2013-02-15 12:57:56 -0600

    Checkpoing before merge w/master

--- src/chapter_1/section_3/linked_list_exercises.rb
+++ src/chapter_1/section_3/linked_list_exercises.rb
@@ -8,66 +8,66 @@ module Chapter1
   module Section3
     class LinkedListExercises
 
-		def initialize
+      def initialize
 
-		end
+      end
 
-      	# Write a method delete() that takes an int argument k and deletes the kth
-      	# element in a linked list, if it exists.
-      	# Assume input is always a string
-     	# Examples:
-		def delete_e31(nodo,input)
+      # Write a method delete() that takes an int argument k and deletes the kth
+      # element in a linked list, if it exists.
+      # Assume input is always a string
+      # Examples:
+      def delete_e31(nodo,input)
 
-			while nodo.next != nil
-					if nodo.value == input
-						nodo.value = nodo.next.value
-						nodo.next = nodo.next.next
-					else
-				 		nodo = nodo.next
-					end
-			end #while ends
-		 end
+        while nodo.next != nil
+          if nodo.value == input
+            nodo.value = nodo.next.value
+            nodo.next = nodo.next.next
+          else
+            nodo = nodo.next
+          end
+        end #while ends
+      end
 
-		#Write a method find() that takes a linked list and a string key as
-		#arguments and returns true if some node in the list has key as its item field, false otherwise.
-      	# Assume input is always a string
-		def find_e32(nodo,input)
-			while nodo.next != nil
-				if nodo.value == input
-					return true
-				end
-				nodo = nodo.next
-			end
-			return false
-		end
+      #Write a method find() that takes a linked list and a string key as
+      #arguments and returns true if some node in the list has key as its item field, false otherwise.
+      # Assume input is always a string
+      def find_e32(nodo,input)
+        while nodo.next != nil
+          if nodo.value == input
+            return true
+          end
+          nodo = nodo.next
+        end
+        return false
+      end
 
-    # Write a method delete() that takes an int argument k and deletes
-    #the kth element in a linked list, if it exists.
-		def reverse_iter_e33(root)
-			previous_node = nil
-			current = root
-			while current != nil
-				next_node = current.next
-				current.next = previous_node
-				previous_node = current
-				current = next_node
-			end
-			return previous_node
-		end
+      # Write a method delete() that takes an int argument k and deletes
+      #the kth element in a linked list, if it exists.
+      def reverse_iter_e33(root)
+        previous_node = nil
+        current = root
+        while current != nil
+          next_node = current.next
+          current.next = previous_node
+          previous_node = current
+          current = next_node
+        end
+        return previous_node
+      end
 
-    # Write a method delete() that takes an int argument k and deletes
-    #the kth element in a linked list, if it exists.
-		def reverse_recur_e33(root)
-			return nil if root.nil?
+      # Write a method delete() that takes an int argument k and deletes
+      #the kth element in a linked list, if it exists.
+      def reverse_recur_e33(root)
+        return nil if root.nil?
 
-			return root if root.next.nil?
+        return root if root.next.nil?
 
-			second = root.next
-			rest = reverse_recur_e33(second)
-			second.next = root
-			root.next = nil
-			return rest
-		end
+        second = root.next
+        rest = reverse_recur_e33(second)
+        second.next = root
+        root.next = nil
+        return rest
+      end
 
     end
   end
